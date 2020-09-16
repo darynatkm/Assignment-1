fb7c828b8e612c1b0e94e3c01377f63df3dedd2a
darynatkm <65510511+darynatkm@users.noreply.github.com>
2020-09-16 22:09:33 +0200

    Create Question1.java

---
+++ Question1.java
@@ -0,0 +1,93 @@
+\\Questions1\\
+public class Question1 {
+	public static void main(String[] args) { 
+		System.out.println("I am ready to be a Java pro :)");
+	}
+}
+
+\\Questions2\\
+public class Question2 {
+	public static void main(String[] args) {
+		System.out.println("Turing"+","+" "+"Alan");
+}
+}
+
+\\Questions3\\
+public class Question3 {
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		System.out.println("5");
+		System.out.println("6");
+		System.out.println("7");
+		System.out.println("8");
+		System.out.println("9");
+		System.out.println("10");
+	}
+}
+
+\\Questions4\\
+public class Main {
+
+	public static void main(String[] arg) {
+
+		System.out.println("Q");
+	}
+}
+
+\\Questions5\\
+public class Question5 {
+	public static void main(String[] args) {
+		System.out.println("*******");
+		System.out.println("**   **");
+		System.out.println("* * * *");
+		System.out.println("*  *  *");
+		System.out.println("* * * *");
+		System.out.println("**   **");
+		System.out.println("*******");
+	}
+}
+\\Questions6\\
+public class Question6 {
+	public static void main(String[] args) {
+		System.out.println("   ^    ");
+		System.out.println("  / \\  ");
+		System.out.println(" /   \\  ");
+		System.out.println("/     \\ ");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("|      |");
+		System.out.println("________");
+}
+}
+
+\\Questions7\\
+public class Question7 {
+public static void main(String[] args){
+	System.out.println(" /\\  /\\");
+	System.out.println("/__\\/__\\");
+	System.out.println("I am from \"Germany\"");
+	System.out.println("How is your day going on,\'good\' or \'bad\'?");
+	}
+}
+
+\\Questions8\\
+public class Question8 {
+	public static void main(String[] args) {
+	System.out.println("******         ***********");
+	System.out.println("**   **        *    **   *");
+	System.out.println("**    **            **    ");
+	System.out.println("**     **           **    ");
+	System.out.println("**     **           **    ");
+	System.out.println("**     **           **    ");
+	System.out.println("**    **            **    ");
+	System.out.println("**   **             **    ");
+	System.out.println("*****             ******  ");
+}
+}
